834328d3a3c9d6e875536c187c1492227de028a5
Akash Mane <46684836+akashmane12598@users.noreply.github.com>
2020-09-27 14:09:38 +0530

    Update Program.cs

--- pull/Program.cs
+++ pull/Program.cs
@@ -6,7 +6,6 @@ namespace pull
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello People!");
             Console.WriteLine("Google");
             Console.WriteLine("Microsoft");
             Console.WriteLine("This is the slave remote part");
