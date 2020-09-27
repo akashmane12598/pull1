68bddc30b41a13bc84c8e0347960bd6cfa88fb2a
Akash <akashmane12598@gmail.com>
2020-09-27 14:36:07 +0530

    [Akash]

--- pull/Program.cs
+++ pull/Program.cs
@@ -8,14 +8,10 @@ namespace pull
         {
             Console.WriteLine("Google");
             Console.WriteLine("Microsoft");
-<<<<<<< HEAD
             Console.WriteLine("This is the slave remote part");
             Console.WriteLine("Hello India");
             Console.WriteLine("Hitman");
             Console.WriteLine("Virat Kohli");
-=======
-            Console.WriteLine("This is the slave local part2");
->>>>>>> slave
         }
     }
 }
