7ef1a23fdd338776462517c19cbeebe3c61d615a
Akash <akashmane12598@gmail.com>
2020-09-27 13:57:23 +0530

    [Akash]Refracotor

--- pull/Program.cs
+++ pull/Program.cs
@@ -9,6 +9,8 @@ namespace pull
             Console.WriteLine("Google");
             Console.WriteLine("Microsoft");
             Console.WriteLine("This is the slave remote part");
+            Console.WriteLine("Hello India");
+            Console.WriteLine("Hitman");
         }
     }
 }
